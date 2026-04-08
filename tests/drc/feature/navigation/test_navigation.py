dad191114529ae4443fdb4f2cb4d212280a3d0b5
nohungry <tzuyang1991@gmail.com>
2026-04-09 01:53:24 +0800

    fix(drc/navigation): 補上分類頁載入的 verify 截圖

--- tests/drc/feature/navigation/test_navigation.py
+++ tests/drc/feature/navigation/test_navigation.py
@@ -6,6 +6,7 @@ import re
 import pytest
 from playwright.sync_api import Page, expect
 from pages.drc.home_page import HomePage
+from utils.screenshot_helper import get_screenshotter
 
 
 @pytest.mark.p1
@@ -19,19 +20,25 @@ class TestNavigation:
         """點擊真人分類後頁面正常載入"""
         page = class_logged_in_page
         home = HomePage(page)
+        sh = get_screenshotter(page)
         home.click_nav_item("真人")
         expect(page).to_have_url(re.compile("Categories/casino"), timeout=8000)
+        if sh: sh.full_page("verify_真人分類頁載入")
 
     def test_slots_page_loads(self, class_logged_in_page: Page, go_home):
         """點擊電子分類後頁面正常載入"""
         page = class_logged_in_page
         home = HomePage(page)
+        sh = get_screenshotter(page)
         home.click_nav_item("電子")
         expect(page).to_have_url(re.compile("Categories/slots"), timeout=8000)
+        if sh: sh.full_page("verify_電子分類頁載入")
 
     def test_fishing_page_loads(self, class_logged_in_page: Page, go_home):
         """點擊捕魚分類後頁面正常載入"""
         page = class_logged_in_page
         home = HomePage(page)
+        sh = get_screenshotter(page)
         home.click_nav_item("捕魚")
         expect(page).to_have_url(re.compile("Categories/fishing"), timeout=8000)
+        if sh: sh.full_page("verify_捕魚分類頁載入")
